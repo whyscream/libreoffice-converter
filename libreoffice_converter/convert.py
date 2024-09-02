45ee41b8bd0369048d58bcc0d77ad59db67eebfe
Tom Hendrikx <tom@whyscream.net>
2024-09-03 00:24:03 +0200

    Make converted file handling simpler: don't parse stdout but read filesystem

--- libreoffice_converter/convert.py
+++ libreoffice_converter/convert.py
@@ -4,6 +4,7 @@ import re
 import subprocess
 import tempfile
 from datetime import datetime
+from pathlib import Path
 
 logger = logging.getLogger(__name__)
 
@@ -15,15 +16,17 @@ def convert_file(file, format_to):
     """
     # Create a temporary directory to store the files
     app_tempdir = os.getenv("APP_TEMP_DIR", tempfile.gettempdir())
-    app_keep_files = os.getenv("APP_DELETE_FILES", "true").lower() == "true"
+    app_delete_files = os.getenv("APP_DELETE_FILES", "true").lower() == "true"
     now = datetime.now().strftime("%Y%m%d-%H%M%S-")
 
-    with tempfile.TemporaryDirectory(dir=app_tempdir, prefix=now, delete=app_keep_files) as temp_dir:
+    with tempfile.TemporaryDirectory(dir=app_tempdir, prefix=now, delete=app_delete_files) as temp_dir:
         # Save the original file to the temporary directory
-        original_file_path = os.path.join(temp_dir, file.filename)
+        original_file_path = Path(temp_dir) / "original" / file.filename
+        original_file_path.parent.mkdir(parents=True, exist_ok=True)
         file.save(original_file_path)
 
         # Run the libreoffice command to convert the file
+        outdir = Path(temp_dir) / "converted"
         process = subprocess.run(
             [
                 "libreoffice",
@@ -31,8 +34,8 @@ def convert_file(file, format_to):
                 "--convert-to",
                 format_to,
                 "--outdir",
-                temp_dir,
-                original_file_path,
+                str(outdir),
+                str(original_file_path),
             ],
             capture_output=True,
             check=True,
@@ -40,19 +43,23 @@ def convert_file(file, format_to):
         # Output of the command contains the path to the file:
         # b'convert /tmp/tmpm0nkb7y8/test.odt -> /tmp/tmpm0nkb7y8/test.pdf using filter : writer_pdf_Export\n'
 
-        # Get the converted file name from the output
-        if match := re.match(r"convert (.*) -> (.*) using filter", process.stdout.decode()):
-            converted_file_path = match.group(2)
-            logger.warning(f"Converted file: {file.filename}: {converted_file_path}")
-
-            # Open the converted file and return it, so the temporary directory can be cleaned up
-            converted_file = open(converted_file_path, "rb")
-            return converted_file
-        else:
+        converted_files = list(outdir.iterdir())
+        if not converted_files:
             stdout = process.stdout.decode()
             stderr = process.stderr.decode()
-            logger.warning(f"Conversion failed: {file.filename}: {format_to=} {stdout=} {stderr=}")
             if "Error: no export filter for" in stderr:
-                raise Exception(f"Conversion failed, no export filter found for the format: {format_to}")
-            else:
-                raise Exception("Conversion failed")
+                raise Exception("No export filter found for the requested format and the uploaded file")
+
+            logger.warning(f"Conversion failed: {file.filename}: {format_to=} {stdout=} {stderr=}")
+            raise Exception("Conversion failed")
+
+        elif len(converted_files) == 1:
+            # Return the file directly if there is only one
+            converted_file = converted_files[0]
+            logger.warning(f"Converted file: {file.filename}: {converted_file}")
+            return converted_file.open("rb")
+
+        else:
+            # Multiple files (f.i. xhtml + extracted images)
+            logger.warning(f"Multiple converted files found: {file.filename}: {[f.name for f in converted_files]}")
+            raise Exception("Conversion failed, multiple converted files found")
